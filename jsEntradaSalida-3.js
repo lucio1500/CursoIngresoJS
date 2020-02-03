b8883a66a05dd1a3a1e1013737b8056c2f5956fc
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2020-02-03 12:27:31 -0300

    Add files via upload

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+    var nombre
+    //asigno a la variable nombre el valor escrito en la caja 
+    nombre =document.getElementById("elNombre").value;
+    alert (nombre);
+}
+
+
