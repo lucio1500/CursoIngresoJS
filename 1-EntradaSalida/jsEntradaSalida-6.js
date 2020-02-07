7dcbc564a14913ccd0aa98b401233c809ee72e06
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2020-02-07 10:52:24 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    
+    var num1,num2,resultado;
+  // ASI  num1 = document.getElementById("numeroUno").value;
+  //  num1=parseInt(num1);
+  // sino mas rapido y como se ""debe""
+    num1=parseInt(document.getElementById("numeroUno").value);
+    num2 = document.getElementById("numeroDos").value;
+    num2=parseInt(num2);
+    resultado = num1 + num2;
+    alert ("la suma es "+resultado);
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,45 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var num1,num2,resultado;
+    num1 = document.getElementById("numeroUno").value;
+    num1=parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2=parseInt(num2);
+    resultado = num1 + num2;
+    alert ("la suma es "+resultado);
 }
 
 function restar()
 {
-	
+	var num1,num2,resultado;
+    num1 = document.getElementById("numeroUno").value;
+    num1=parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2=parseInt(num2);
+    resultado = num1 - num2;
+    alert ("la resta es "+resultado);
 }
 
 function multiplicar()
 { 
-	
+	var num1,num2,resultado;
+    num1 = document.getElementById("numeroUno").value;
+    num1=parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2=parseFloat(num2);
+    resultado = num1 * num2;
+    alert ("la multiplicacion es "+resultado);
 }
 
 function dividir()
 {
-	
+	var num1,num2,resultado;
+    num1 = document.getElementById("numeroUno").value;
+    num1=parseInt(num1);
+    num2 = document.getElementById("numeroDos").value;
+    num2=parseFloat(num2);
+    resultado = num1 / num2;
+    alert ("la division es "+resultado);
 }
 
