48e61856e631017014ba5310358052d12a385814
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2020-02-17 11:26:38 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,9 +1,9 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
-	while(!(numero>=0&&numero<=9)){
-		numero = prompt ("numero incorrecto ingrese un numero entre 0 y 10");
+	var numero = parseInt( prompt("ingrese un número entre 0 y 10."));
+	while(numero<0||numero>9||isNaN(numero)){
+		numero = parseInt( prompt ("numero incorrecto ingrese un numero entre 0 y 10"));
 	}
 	document.getElementById("Numero").value=numero;
 }//FIN DE LA FUNCIÓN
