4827e7e250ab0376806d921b3671e5976ef11f65
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2020-02-17 00:15:05 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 	alert('iteración while');
-
+	var num = 0;
+	num = parseInt(num);
+	while(num<10){
+		num= num + 1;
+		alert (num);
+	}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 	alert('iteración while');
-
+	var num = 11;
+	num = parseInt(num);
+	while(num>1){
+		num= num - 1;
+		alert (num);
+	}
 
 }//FIN DE LA FUNCIÓN
