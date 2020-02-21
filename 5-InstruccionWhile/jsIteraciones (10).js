0315af91dc6beb256ec6a8e48ade4506ccab833e
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2020-02-21 12:18:00 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,47 @@
 function mostrar()
 {
-
+//declarar contadores y variables 
 	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var contadorPositivo=0;
+	var contadorNegativo=0;
+	var contadorCero=0;
+	var contadorPares=0;
+	var numero;
+	var positivo
+	var negativo 
+	var promPositivo
+	var promNegativo;
+	var diferencia;
+	var respuesta;
 
-	while(respuesta!="no")
-	{
-		
+	do{
+		numero=parseInt (prompt("Ingrese un numero"));
+		while(isNaN(numero)){
+		numero=parseInt (prompt("Eso no es un numero. Ingrese un numero")); 
+		}
 	
-	}
+			if(numero>0)
+			{
+				positivo=positivo+numero;
+				contadorPositivo++;
+			}
+			else if (numero<0)
+			{
+				negativo=negativo+numero;
+				contadorNegativo++;
+			}
+			else
+			{
+				contadorCero++;
+			}
+
+			
+	
+	//diferencia: cantidad de postivos menos cantidad de negativos
+	
+	respuesta=prompt("Ingrese 'si' para seguir ingresando numeros y 'no' para terminar: ").toLowerCase();
+	
+	}while(respuesta=="si");
 
 
 
