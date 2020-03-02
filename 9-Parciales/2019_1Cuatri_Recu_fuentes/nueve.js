408c74e3a86965a2a9ac7cd907e603775b873b0d
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2020-03-02 12:20:45 -0300

    Add files via upload

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -17,53 +17,74 @@ var contmayor=0;
 var contmenor=0;
 var contAdoles=0;
 var cantnotas=0;
+var sexonotamax;
+var notamax=0;
 //informar el sexo y nombre de la mayor nota de los adolescentes
 do{
     edad=parseInt( prompt("Ingrese una edad"));
     while(isNaN(edad)||edad<0){
         edad=parseInt( prompt("Incorrecto, Ingrese una edad"));
     }
+
     nombre=prompt("Ingrese un nombre ");
     while(!((nombre>='A'&&nombre<='Z')||(nombre>='a'&&nombre<='z'))){
         nombre=prompt("Incorrecto, Ingrese un nombre ");
     }
 
-
     sexo=prompt("Ingrese 'm' si es hombre y 'f' si es mujer: ").toLowerCase();
     while(!(sexo=='f'||sexo=='m')){
         sexo=prompt("Ingrese 'm' si es hombre y 'f' si es mujer: ").toLowerCase();
     }
+
     nota=parseInt( prompt("Ingrese una nota"));
     while(isNaN(nota)){
         nota=parseInt( prompt("Incorrecto, Ingrese una nota"));
     }
+
     cantnotas++;
+
     if (nota>=4){
         cantvaraprovados++;
     }
+
+
     if (edad>=18){
     contmayor++;
     promediomayor=nota/contmayor;
     }
     else {
         contmenor++;
-        promediomenor=nota/cantnotas;
+        
         if(edad<=17&&edad>=13){
         contAdoles++;
-        promedioAdoles=nota/contAdoles;
+        notamax=nota;
+        if (flag==0||nota>notamax){
+            notamax=nota;
+            sexonotamax=sexo;
+            nombremax=nombre;
+            flag=1;
+        }
+        notamax=nota;
+
         }
     }
 
     if (sexo=='f'){
         cantFem++;
-        promediofem=nota/cantFem;
+        
     }
     else{
         cantvar++;
         promediomasc=nota/cantvar;
     }
+    
+
 
 }while(respuesta=='s');
+
+promediomenor=nota/cantnotas;
+promedioAdoles=nota/contAdoles;
+promediofem=nota/cantFem;
 
 
 }
